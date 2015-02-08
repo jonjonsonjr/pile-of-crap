033a52d6bbe67202e2f955ca7a065fb7a279a665
Jon Johnson <jonjonsonjr@gmail.com>
2015-02-07 21:10:30 -0500

    oops

--- app.js
+++ app.js
@@ -78,7 +78,6 @@ router.post('/api/diamond', function (req, res) {
 
 router.get('/api/players/:username', function (req, res) {
   var username = req.params.username;
-  res.send(username);
 
   Games.getLatestGameId(function (err, game_id) {
     Games.getParticipants(game_id, function (err, results) {
